b9700256344a754ea3fc5b5d91854d6c1bf20fdb
Jacob Leigh <jacoblaurenceleigh@berkeley.edu>
2022-11-22 15:38:23 -0800

    cleanup and greedy start

---
+++ greedy.py
@@ -0,0 +1,13 @@
+from starter import *
+import networkx as nx
+
+# INCOMPLETE
+def greedy(G, k):
+    n = G.number.of.nodes()
+    edges = G.edges(data=True)
+    edges.sort(lambda x: x['weight'])
+
+    return team
+
+G = read_input('./inputs/small1.in')
+edges = G.edges(data=True)
