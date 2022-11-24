0a35813161dccece39a7a66f8312fd0c5cc49ec0
Jacob Leigh <jacoblaurenceleigh@berkeley.edu>
2022-11-23 16:22:24 -0800

    commented partition generator

--- greedy.py
+++ greedy.py
@@ -1,13 +1,20 @@
 from starter import *
 import networkx as nx
+from networkx.algorithms import approximation
 
 # INCOMPLETE
 def greedy(G, k):
     n = G.number.of.nodes()
+    G_copy = nx.complete_graph(n)
     edges = G.edges(data=True)
     edges.sort(lambda x: x['weight'])
-
-    return team
+    team_size = n / k
+    remainder = n - team_size
+    while nx.number_connected_components(G) > k:
+        longest_edge = edges.pop()
+        G.remove_edge(longest_edge)
+    
+    
 
 G = read_input('./inputs/small1.in')
 edges = G.edges(data=True)
--- partition_generator.py
+++ partition_generator.py
@@ -1,9 +1,8 @@
 import k_partition
 
-# sizes = ['small', 'medium', 'large']
-sizes = ['large']
+sizes = ['small', 'medium', 'large']
 
 for size in sizes:
-    for num in range(179, 261):
-        for k in range (2, 30):
+    for num in range(1, 261):   # FROM small/medium/large 1 to 260
+        for k in range (2, 30): # FROM k = 2 to 29
             k_partition.main(size, num, k)
